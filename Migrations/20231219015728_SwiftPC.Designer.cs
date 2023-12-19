710ad9da01454b7c51725aa0133cfba75536376a
Jovanni Feliz <feliz.jovanni@gmail.com>
2023-12-18 19:57:55 -0600

    added Migrations

--- Migrations/20231219015728_SwiftPC.Designer.cs
+++ Migrations/20231219015728_SwiftPC.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CAPSTONE_Swift_Server;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CAPSTONE_Swift_Server.Migrations
 {
     [DbContext(typeof(SwiftDbContext))]
-    partial class SwiftDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231219015728_SwiftPC")]
+    partial class SwiftPC
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
